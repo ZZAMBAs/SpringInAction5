e8d916e33e1abe4ed2ba4f23035d7dce285e9799
ZZAMBAs <rksidksrksi@naver.com>
2022-10-18 23:16:21 +0900

    1.3.2 뷰 정의하기

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -3,6 +3,7 @@ package tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 전체 예제 소스 코드: https://github.com/Jpub/SpringInAction5
 @SpringBootApplication
 public class TacoCloudApplication {
 
