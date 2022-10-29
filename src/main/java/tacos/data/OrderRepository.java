14150f60b94c03c1d02ae2d7a701f431f44d3127
ZZAMBAs <rksidksrksi@naver.com>
2022-10-29 17:56:14 +0900

    3.2.4 JPA 리퍼지터리 커스터마이징하기

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    // List<Order> findByDeliveryZip(String deliveryZip); // JPA Repository 커스터마이징.
+    // https://www.baeldung.com/spring-data-derived-queries, https://jithub.tistory.com/342 의 맨 아래 챕터.
 }
